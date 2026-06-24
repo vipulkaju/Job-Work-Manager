c3c2ea73a71dfdd839f98519e5648d0793f7bd64
vipulkaju <vipul.siddhapura90@gmail.com>
2026-06-24 16:24:03 +0530

    refactor: hardcode firebase configuration
    Remove dependency on external config file by embedding firebase configuration directly into the initialization module for better portability.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,11 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
-import config from "../../firebase-applet-config.json";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAaKoiGdALerLQMms43Qex8uyVfb8O6qqg",
+  authDomain: "jobwork-87bd5.firebaseapp.com",
+  projectId: "jobwork-87bd5",
+  storageBucket: "jobwork-87bd5.firebasestorage.app",
+  messagingSenderId: "624256200279",
+  appId: "1:624256200279:web:2ee2b1f59af8d3327f7d12",
+  measurementId: "G-QPNWV2EX60"
+};
 
 // Initialize Firebase
-const app = initializeApp(config);
-export const db = getFirestore(app, config.firestoreDatabaseId);
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 
